a0d6269844419eff4d7c97d0d787f1c5b9e61239
ydumburs <45432538+ydumburs@users.noreply.github.com>
2020-08-02 12:22:31 +0100

    Added BasicVideoApp

---
+++ Android/VideoAPI/BasicVideoApp/app/src/main/java/com/example/opentok1/MainActivity.java
@@ -0,0 +1,210 @@
+package com.example.opentok1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import com.opentok.android.Session;
+import com.opentok.android.Stream;
+import com.opentok.android.Publisher;
+import com.opentok.android.PublisherKit;
+import com.opentok.android.Subscriber;
+import com.opentok.android.OpentokError;
+import androidx.annotation.NonNull;
+import android.Manifest;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.opengl.GLSurfaceView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity
+        implements  Session.SessionListener,
+        PublisherKit.PublisherListener {
+
+// If you want to hardcode your authentication, use here
+//    private static String API_KEY = "YOUR_API_KEY";
+//    private static String SESSION_ID = "YOUR_SESSION_ID";
+//    private static String TOKEN = "YOUR_TOKEN";
+    private static String API_KEY;
+    private static String SESSION_ID;
+    private static String TOKEN;
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SETTINGS_SCREEN_PERM = 123;
+    private static final int RC_VIDEO_APP_PERM = 124;
+    private Session mSession;
+    private FrameLayout mPublisherViewContainer;
+    private FrameLayout mSubscriberViewContainer;
+    private Publisher mPublisher;
+    private Subscriber mSubscriber;
+    private boolean flagVideo = true;
+    private boolean flagAudio = true;
+
+    public void fetchSessionConnectionData() {
+        RequestQueue reqQueue = Volley.newRequestQueue(this);
+        reqQueue.add(new JsonObjectRequest(Request.Method.GET,
+                "https://YOUR_HEROKU_APP_NAME.herokuapp.com" + "/session",
+                null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    API_KEY = response.getString("apiKey");
+                    SESSION_ID = response.getString("sessionId");
+                    TOKEN = response.getString("token");
+
+                    Log.i(LOG_TAG, "AUTH_API_KEY: " + API_KEY);
+                    Log.i(LOG_TAG, "AUTH_SESSION_ID: " + SESSION_ID);
+                    Log.i(LOG_TAG, "AUTH_TOKEN: " + TOKEN);
+
+                    mSession = new Session.Builder(MainActivity.this, API_KEY, SESSION_ID).build();
+                    mSession.setSessionListener(MainActivity.this);
+                    mSession.connect(TOKEN);
+
+                } catch (JSONException error) {
+                    Log.e(LOG_TAG, "Web Service error: " + error.getMessage());
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e(LOG_TAG, "Web Service error: " + error.getMessage());
+            }
+        }));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        requestPermissions();
+
+        // Identify buttons
+        Button button_video = findViewById(R.id.button_video);
+
+        button_video.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (flagVideo) {
+                    mPublisher.setPublishVideo(false);
+                    flagVideo = false;
+                    Log.v("MainActivity", "video disabled");
+                } else {
+                    mPublisher.setPublishVideo(true);
+                    flagVideo = false;
+                    Log.v("MainActivity", "video enabled");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @AfterPermissionGranted(RC_VIDEO_APP_PERM)
+    private void requestPermissions() {
+        String[] perms = { Manifest.permission.INTERNET, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO };
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            // initialize view objects from your layout
+            mPublisherViewContainer = (FrameLayout) findViewById(R.id.publisher_container);
+            mSubscriberViewContainer = (FrameLayout) findViewById(R.id.subscriber_container);
+
+            // initialize and connect to the session
+            fetchSessionConnectionData();
+
+        } else {
+            EasyPermissions.requestPermissions(this, "This app needs access to your camera and mic to make video calls", RC_VIDEO_APP_PERM, perms);
+        }
+    }
+
+//    If you want to hardcode your authentication, use here instead of the above requestPermissions() method
+//    @AfterPermissionGranted(RC_VIDEO_APP_PERM)
+//    private void requestPermissions() {
+//        String[] perms = { Manifest.permission.INTERNET, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO };
+//        if (EasyPermissions.hasPermissions(this, perms)) {
+//            // initialize view objects from your layout
+//            mPublisherViewContainer = (FrameLayout) findViewById(R.id.publisher_container);
+//            mSubscriberViewContainer = (FrameLayout) findViewById(R.id.subscriber_container);
+//
+//            // initialize and connect to the session
+//            mSession = new Session.Builder(this, API_KEY, SESSION_ID).build();
+//            mSession.setSessionListener(this);
+//            mSession.connect(TOKEN);
+//        } else {
+//            EasyPermissions.requestPermissions(this, "This app needs access to your camera and mic to make video calls", RC_VIDEO_APP_PERM, perms);
+//        }
+//    }
+
+    // SessionListener methods
+    @Override
+    public void onConnected(Session session) {
+        Log.i(LOG_TAG, "Session Connected");
+        mPublisher = new Publisher.Builder(this).build();
+        mPublisher.setPublisherListener(this);
+
+        mPublisherViewContainer.addView(mPublisher.getView());
+
+        if (mPublisher.getView() instanceof GLSurfaceView){
+            ((GLSurfaceView) mPublisher.getView()).setZOrderOnTop(true);
+        }
+
+        mSession.publish(mPublisher);
+    }
+
+    @Override
+    public void onDisconnected(Session session) {
+        Log.i(LOG_TAG, "Session Disconnected");
+    }
+
+    @Override
+    public void onStreamReceived(Session session, Stream stream) {
+        Log.i(LOG_TAG, "Stream Received");
+        if (mSubscriber == null) {
+            mSubscriber = new Subscriber.Builder(this, stream).build();
+            mSession.subscribe(mSubscriber);
+            mSubscriberViewContainer.addView(mSubscriber.getView());
+        }
+    }
+
+    @Override
+    public void onStreamDropped(Session session, Stream stream) {
+        Log.i(LOG_TAG, "Stream Dropped");
+        if (mSubscriber != null) {
+            mSubscriber = null;
+            mSubscriberViewContainer.removeAllViews();
+        }
+    }
+
+    @Override
+    public void onError(Session session, OpentokError opentokError) {
+        Log.e(LOG_TAG, "Session error: " + opentokError.getMessage());
+    }
+
+    // PublisherListener methods
+
+    @Override
+    public void onStreamCreated(PublisherKit publisherKit, Stream stream) {
+        Log.i(LOG_TAG, "Publisher onStreamCreated");
+    }
+
+    @Override
+    public void onStreamDestroyed(PublisherKit publisherKit, Stream stream) {
+        Log.i(LOG_TAG, "Publisher onStreamDestroyed");
+    }
+
+    @Override
+    public void onError(PublisherKit publisherKit, OpentokError opentokError) {
+        Log.e(LOG_TAG, "Publisher error: " + opentokError.getMessage());
+    }
+}
